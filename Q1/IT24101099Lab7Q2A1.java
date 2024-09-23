fec59098a27f2c931e0f5b290aeb6a4abc9f7afa
it24101099 <it24101099@my.sliit.lk>
2024-09-23 23:45:42 +0530

    7QA2

---
+++ Q1/IT24101099Lab7Q2A1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class IT24101099Lab7Q2A1{
+	public static void main(String[]args){
+		int i,j;
+		for (j=1; j<=3; j++){//Outer loop
+		
+			for(i=1; i<=5; i++){//Inner loop
+				System.out.print("$");
+		}
+		System.out.print("\n");
+	}
+		
+}
+}
+	
+	
